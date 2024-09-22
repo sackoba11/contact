195a42e3f82c19f8c6b06d2e77bd8c1534906cdb
sackoba <badrasacko11@gmail.com>
2024-09-22 14:41:28 +0000

    update concept to add, save, update in json file

--- lib/repository_contacts/contacts_impl.dart
+++ lib/repository_contacts/contacts_impl.dart
@@ -57,11 +57,10 @@ class ContactsImpl implements ContactRepository {
 
   @override
   void displayContacts() {
-    final contactList = getAllContacts();
-    if (contactList.isEmpty) {
+    if (contacts.isEmpty) {
       print('Aucun contact enregistré.');
     } else {
-      for (var i = 0; i < contactList.length; i++) {
+      for (var i = 0; i < contacts.length; i++) {
         print('${i + 1}. ${contacts[i]}');
       }
     }
@@ -94,59 +93,27 @@ class ContactsImpl implements ContactRepository {
 
   @override
   void deletteContact() {
-    final file = File(fileName);
-
-    if (file.existsSync()) {
-      // Lire le contenu actuel du fichier
-      List<String> lines = file.readAsLinesSync();
-      // Afficher le contenu actuel avec des numéros de ligne
-      print('Contenu actuel du fichier:');
-      if (lines.isNotEmpty) {
-        for (int i = 0; i < lines.length; i++) {
-          print('${i + 1}: ${lines[i]}');
-        }
-      } else {
-        print("Aucun contact trouvé. Veuillez ajouter des contacts !");
-        return;
-      }
-
-      // Demander à l'utilisateur quelle(s) ligne(s) supprimer
-      stdout.write(
-          'Entrez les numéros des lignes à supprimer (séparés par des virgules, ex: 1,3,5) : ');
-      String input = stdin.readLineSync() ?? '';
-
-      List<int> linesToDelete = input
-          .split(',')
-          .map((s) => int.tryParse(s.trim()) ?? 0)
-          .where((n) => n > 0 && n <= lines.length)
-          .toList();
-
-      if (linesToDelete.isEmpty) {
-        print('Aucune ligne valide à supprimer.');
-        return;
-      }
-
-      // Trier les numéros de ligne dans l'ordre décroissant pour éviter les problèmes d'index
-      linesToDelete.sort((a, b) => b.compareTo(a));
-
-      // Supprimer les lignes spécifiées
-      for (int lineNumber in linesToDelete) {
-        lines.removeAt(lineNumber - 1);
-        print('Ligne $lineNumber supprimée avec succès.');
-      }
-
-      // Écrire le contenu modifié dans le fichier
-      file.writeAsStringSync(lines.join('\n'));
-    } else {
-      print("Veuillez réessayer.");
+    if (contacts.isEmpty) {
+      print('Aucun contact à supprimer.');
       return;
     }
+    displayContacts();
+    // Demander à l'utilisateur quelle(s) ligne(s) supprimer
+    stdout.write('Entrez le numéro du contact à supprimer : ');
+    final input = int.tryParse(stdin.readLineSync() ?? '');
+    if (input == null || input < 1 || input > contacts.length) {
+      print('Choix invalide. veuillez réessayer !');
+      return;
+    }
+    final delettecontact = contacts.removeAt(input - 1);
+    saveContacts();
+    print('Contact ${delettecontact.toString()} supprimé avec succès.');
+    return;
   }
 
   @override
   void updateContact() {
-    final contactList = getAllContacts();
-    if (contactList.isNotEmpty) {
+    if (contacts.isNotEmpty) {
       // Afficher le contenu actuel avec des numéros de ligne
       displayContacts();
     } else {
@@ -155,15 +122,15 @@ class ContactsImpl implements ContactRepository {
     }
     // Demander à l'utilisateur quelle ligne modifier
     stdout.write(
-        'Entrez le numéro de la ligne à modifier (1-${contactList.length}) : ');
+        'Entrez le numéro de la ligne à modifier (1-${contacts.length}) : ');
     int lineNumber = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
 
-    if (lineNumber < 1 || lineNumber > contactList.length) {
+    if (lineNumber < 1 || lineNumber > contacts.length) {
       print('Numéro de ligne invalide.');
       return;
     }
     // Afficher la ligne actuelle
-    var contact = contactList[lineNumber - 1];
+    var contact = contacts[lineNumber - 1];
     print('Modification du contact : ${contact.toString()}');
     print('Laissez vide pour conserver la valeur actuelle.');
 
@@ -172,19 +139,16 @@ class ContactsImpl implements ContactRepository {
     if (newFirstName?.isNotEmpty == true) {
       contact = contact.copyWith(firstName: newFirstName!);
     }
-
     stdout.write('Nouveau prénom (${contact.lastName}): ');
     final newLastName = stdin.readLineSync();
     if (newLastName?.isNotEmpty == true) {
       contact = contact.copyWith(lastName: newLastName!);
     }
-
     stdout.write('Nouveau numéro de téléphone (${contact.phoneNumber}): ');
     final newPhoneNumber = stdin.readLineSync();
     if (newPhoneNumber?.isNotEmpty == true) {
       contact = contact.copyWith(phoneNumber: newPhoneNumber!);
     }
-
     stdout.write('Nouvel email (${contact.email ?? "Non défini"}): ');
     final newEmail = stdin.readLineSync();
     if (newEmail?.isNotEmpty == true) {
@@ -193,9 +157,9 @@ class ContactsImpl implements ContactRepository {
       contact = contact.copyWith(email: null);
     }
     print(contact.toJson());
+    contacts[lineNumber - 1] = contact;
     saveContacts();
     print('Contact modifié avec succès.');
-
     return;
   }
 }
